95932ddf2b9b88ee653f1bea70392b7ae29717b5
Luciano Martins <zicadopv@gmail.com>
2020-11-30 09:03:39 -0300

    Finalizando exercise: Arithmetic Operators close #5

---
+++ test_example/arithmeticOperators.py
@@ -0,0 +1,15 @@
+def arithmetic_operators(a, b, operator=None):
+    """
+
+    :param a: int
+    :param b: int
+    :return: int
+
+    >>> arithmetic_operators(3,5)
+    8
+    -2
+    15
+    """
+    print(a + b)
+    print(a - b)
+    print(a * b)
